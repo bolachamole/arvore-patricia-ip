e2a612491f858277778a69ba741a1b3681d3efb7
bolachamole <atismistic@proton.me>
2025-11-26 12:05:19 -0400

    Insere

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Implementação da árvore PATRICIA para roteamento de endereços IP.
 
 # Objetivo
 Demonstrar como a estrutura de dados em questão pode ser utilizada para a busca na tabela de repasse, prestando atenção ao uso da memória.
+
+# Inspiração
+O método para determinar o skip foi inspirado na proposta feita para o sistema operacional de código aberto [NetBSD](https://wiki.netbsd.org/projects/project/atomic_radix_patricia_trees/), que utiliza operações bit-a-bit AND para calcular o número de zeros antes do primeiro 1 no resultado da comparação entre prefixos.
--- patricia.c
+++ patricia.c
@@ -3,7 +3,7 @@
 
 typedef struct No tipoNo;
 struct No{
-	void* prefixo;
+	uint32_t prefixo;
 	short int skip;
 	tipoNo* pai;
 	tipoNo* esq;
@@ -13,17 +13,13 @@ struct No{
 typedef struct patricia triePatricia;
 struct patricia{
 	tipoNo* raiz;
-	short int tamanho;
-	int num_inserts;
-	int num_buscas;
-	int num_compara;
-	int num_remocoes;
+	int tamanho;
 };
 
-tipoNo* criar_no(void* prefixo){
+tipoNo* criar_no(uint32_t prefixo){
 	tipoNo* novo = malloc(sizeof(tipoNo));
 	novo->prefixo = prefixo;
-	novo->skip = 0;
+	novo->skip = 1;
 	novo->pai = NULL;
 	novo->esq = NULL;
 	novo->dir = NULL;
@@ -34,17 +30,38 @@ triePatricia* cria_arvore(){
 	triePatricia* arvore = malloc(sizeof(triePatricia));
 	arvore->raiz = NULL;
 	arvore->tamanho = 0;
-	arvore->num_inserts = 0;
-	arvore->num_buscas = 0;
-	arvore->num_compara = 0;
-	arvore->num_remocoes = 0;
 	return arvore;
 }
 
-void insere_no(triePatricia* arvore, void* novo){
+short int conta_zeros(uint32_t prefixo){
+	short int quant_zeros = 0;
+	if (prefixo == 0) return 1;
+	if (prefixo & 0xffff0000) prefixo &= 0xffff0000; else quant_zeros += 16;
+    if (prefixo & 0xff00ff00) prefixo &= 0xff00ff00; else quant_zeros += 8;
+    if (prefixo & 0xf0f0f0f0) prefixo &= 0xf0f0f0f0; else quant_zeros += 4;
+    if (prefixo & 0xcccccccc) prefixo &= 0xcccccccc; else quant_zeros += 2;
+    if (prefixo & 0xaaaaaaaa) prefixo &= 0xaaaaaaaa; else quant_zeros += 1;
+	return quant_zeros;
 }
 
-void deleta_no(triePatricia* arvore, void* prefixo){
+int insere_no(triePatricia* arvore, tipoNo* raiz, uint32_t prefixo){
+	tipoNo* novo = criar_no(prefixo);
+	if(raiz == NULL){
+		raiz = novo;
+		arvore->tamanho++;
+		return 0;
+	} else if((raiz->prefixo ^ prefixo) != 0){
+		raiz->skip = conta_zeros(raiz->prefixo ^ prefixo);
+		if(( (prefixo & (1 << (raiz->skip - 1))) >> (raiz->skip - 1) ) == 0){
+			insere_no(arvore, raiz->esq, prefixo);
+		} else{
+			insere_no(arvore, raiz->dir, prefixo);
+		}
+	}
+	return 1;
+}
+
+void deleta_no(triePatricia* arvore, uint32_t prefixo){
 }
 
 void comprime_caminho(triePatricia* arvore){
